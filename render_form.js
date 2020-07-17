e099901a3488b7f3e22427f71061011d7b36a1e3
michaelkoziana <31865561+michaelkoziana@users.noreply.github.com>
2020-07-17 13:03:56 -0500

    Update render_form.js
    formatting

--- render_form.js
+++ render_form.js
@@ -41,5 +41,5 @@ function getInfoFromCWS() {
         }
     }
 
-    document.getElementById("preview-area").value = Name + title;
+    document.getElementById("preview-area").value = '| ' + Name + ' |\n' + title;
 }
