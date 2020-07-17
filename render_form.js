15918ded30ec7818d552d9b22cea97554206c6cf
Chirag Rastogi <rastoc1@cat.com>
2020-07-17 12:06:15 -0500

    Team details added

--- render_form.js
+++ render_form.js
@@ -1,7 +1,15 @@
 var Eva = {cwsId: "evayl2", firstName: "Eva", lastName: "Liu", jobTitle:"Intern", 
     divisionTeam:"SIS 2.0", email:"Liu_Eva_Y@cat.com", phone:""};
+var Chirag = {cwsId: "rastoc1", firstName: "Chirag", lastName: "Rastogi", jobTitle:"Intern", 
+    divisionTeam:"Machine Data Insights", email:"Rastogi_chirag@cat.com", phone:"2179797479"};
+var Michael = {cwsId: "koziamj", firstName: "Michael", lastName: "Koziana", jobTitle:"Intern", 
+    divisionTeam:"Architecture", email:"Koziana_Michael_J@cat.com", phone:"8477560707"};
+var Vaibhav = {cwsId: "makarvv", firstName: "Vaibhav", lastName: "Makar", jobTitle:"Intern", 
+    divisionTeam:"System Architecture", email:"Makar_Vaibhav_V@cat.com", phone:"2175528103"};
+var Catherine = {cwsId: "yuanc3", firstName: "Catherine", lastName: "Yuan", jobTitle:"Intern", 
+    divisionTeam:"Team Aura", email:"Yuan_Catherine@cat.com", phone:""};
 
-var sampleInterns = [Eva];
+var sampleInterns = [Eva,Chirag,Michael,Vaibhav,Catherine];
 
 var orientation = "";
 var Name = "";
