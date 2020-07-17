944eed70c71e29f6a5086b336cb7f79970795d09
Chirag Rastogi <rastoc1@cat.com>
2020-07-17 12:12:38 -0500

    Phone number format changed

--- render_form.js
+++ render_form.js
@@ -1,11 +1,11 @@
 var Eva = {cwsId: "evayl2", firstName: "Eva", lastName: "Liu", jobTitle:"Intern", 
-    divisionTeam:"SIS 2.0", email:"Liu_Eva_Y@cat.com", phone:""};
+    divisionTeam:"SIS 2.0", email:"Liu_Eva_Y@cat.com", phone:"+1.309.675.9620"};
 var Chirag = {cwsId: "rastoc1", firstName: "Chirag", lastName: "Rastogi", jobTitle:"Intern", 
-    divisionTeam:"Machine Data Insights", email:"Rastogi_chirag@cat.com", phone:"2179797479"};
+    divisionTeam:"Machine Data Insights", email:"Rastogi_chirag@cat.com", phone:"+1.217.979.7479"};
 var Michael = {cwsId: "koziamj", firstName: "Michael", lastName: "Koziana", jobTitle:"Intern", 
-    divisionTeam:"Architecture", email:"Koziana_Michael_J@cat.com", phone:"8477560707"};
+    divisionTeam:"Architecture", email:"Koziana_Michael_J@cat.com", phone:"+1.847.756.0707"};
 var Vaibhav = {cwsId: "makarvv", firstName: "Vaibhav", lastName: "Makar", jobTitle:"Intern", 
-    divisionTeam:"System Architecture", email:"Makar_Vaibhav_V@cat.com", phone:"2175528103"};
+    divisionTeam:"System Architecture", email:"Makar_Vaibhav_V@cat.com", phone:"+1.217.552.8103"};
 var Catherine = {cwsId: "yuanc3", firstName: "Catherine", lastName: "Yuan", jobTitle:"Intern", 
     divisionTeam:"Team Aura", email:"Yuan_Catherine@cat.com", phone:""};
 
